fec3704b6a2677a41ee59154cca8302fa51af0a8
Luciano Munhoz Silva <luciano.asimov@gmail.com>
2025-06-19 19:46:50 -0300

    feat: switch Selenium to HtmlAgilityPack

--- Services/CotacaoService.cs
+++ Services/CotacaoService.cs
@@ -1,38 +1,38 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using HtmlAgilityPack;
 using Radar.Models;
 using System.Globalization;
 
 public class CotacaoService
 {
+    private readonly HttpClient _httpClient;
+
+    public CotacaoService()
+    {
+        _httpClient = new HttpClient();
+    }
+
     public async Task<Acao?> ObterCotacoesAsync(string ticker)
     {
-        var options = new ChromeOptions();
-        options.AddArgument("--headless=new");
-        options.AddArgument("--disable-gpu");
-        options.AddArgument("--no-sandbox");
-
-        var service = ChromeDriverService.CreateDefaultService("/usr/local/bin");
-        service.HideCommandPromptWindow = true;
-
-        using var driver = new ChromeDriver(service, options);
-
         try
         {
             var url = $"https://www.infomoney.com.br/cotacoes/b3/acao/{ticker}/";
-            driver.Navigate().GoToUrl(url);
-            await Task.Delay(2000);
+            var html = await _httpClient.GetStringAsync(url);
 
-            // Cotação atual
-            var cotacaoAtualElemento = driver.FindElement(By.CssSelector(
-                "body > div.fill-lightgray.border-b > div > div.row > div.col-12.col-lg-8.order-2.order-lg-1 > div > div.line-info > div.value > p"));
-            var cotacaoAtualTexto = cotacaoAtualElemento.Text.Replace("R$", "").Replace(",", ".").Trim();
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var cotacaoAtualNode = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class,'line-info')]//div[contains(@class,'value')]//p");
+            if (cotacaoAtualNode == null)
+                throw new Exception("Elemento de cotação atual não encontrado");
+
+            var cotacaoAtualTexto = cotacaoAtualNode.InnerText.Replace("R$", "").Replace(".", "").Replace(",", ".").Trim();
             var cotacaoAtual = decimal.Parse(cotacaoAtualTexto, CultureInfo.InvariantCulture);
 
-            // Cotação anterior
-            var cotacaoAnteriorElemento = driver.FindElement(By.CssSelector(
-                "#header-quotes > div.tables > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(2)"));
-            var cotacaoAnteriorTexto = cotacaoAnteriorElemento.Text.Replace("R$", "").Replace(",", ".").Trim();
+            var cotacaoAnteriorNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@id='header-quotes']//table[1]//tr[1]/td[2]");
+            if (cotacaoAnteriorNode == null)
+                throw new Exception("Elemento de cotação anterior não encontrado");
+
+            var cotacaoAnteriorTexto = cotacaoAnteriorNode.InnerText.Replace("R$", "").Replace(".", "").Replace(",", ".").Trim();
             var cotacaoAnterior = decimal.Parse(cotacaoAnteriorTexto, CultureInfo.InvariantCulture);
 
             var acao = new Acao
@@ -42,7 +42,7 @@ public class CotacaoService
                 CotacaoAnterior = cotacaoAnterior
             };
 
-            Console.WriteLine($"[{acao.Ticker}] Atual: {acao.CotacaoAtual} | Anterior: {acao.CotacaoAnterior} | Variação: {acao.VariacaoPercentual:F2}%");
+            Console.WriteLine($"[{acao.Ticker}] Atual: {acao.CotacaoAtual} | Anterior: {acao.CotacaoAnterior}");
 
             return acao;
         }
@@ -50,10 +50,6 @@ public class CotacaoService
         {
             Console.WriteLine($"Erro ao obter cotações para {ticker}: {ex.Message}");
             return null;
-        }
-        finally
-        {
-            driver.Quit();
         }
     }
 }
